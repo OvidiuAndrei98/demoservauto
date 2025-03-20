7decc8d54cd88cb2017b258803058459ded3bd1d
A N D R E I <72840832+OvidiuAndrei98@users.noreply.github.com>
2025-03-20 22:54:03 +0200

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,4 @@
-// import { PrismaClient } from "@prisma/client";
 
-const uri =
- 
-
-// declare global {
-//   var db: MongoClient | undefined;
-// }
-
-// export const db = globalThis.db || new PrismaClient();
 const MongoClient = require("mongodb");
 export const client = new MongoClient(uri);
 
-// if (process.env.NODE_ENV !== "production") {
-//   globalThis.db = db;
-// }
